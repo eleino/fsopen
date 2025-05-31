c65080a452acf80f89e618b1639cd6db9caee6fd
Emilia Leino <leinonemmi@gmail.com>
2025-05-31 21:07:18 +0300

    Tehtävät 1.3-1.5

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,41 +1,60 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
 
 const Header = (props) => (
-    <h1>{props.course}</h1>
+  <h1>{props.course}</h1>
+)
+
+const Content = (props) => {
+  const mapProps = props.parts.map((part, idx) => {
+    return (<Parts key={idx} name={part.name} exercises={part.exercises} />)
+  })
+  return (
+    <>
+      {mapProps}
+    </>
   )
+}
 
-const Content = (props) => (
-  <>
-  <Parts part={props.parts[0]} exercises={props.exercises[0]} />
-  <Parts part={props.parts[1]} exercises={props.exercises[1]} />
-  <Parts part={props.parts[2]} exercises={props.exercises[2]} />
 
-  </>
-)
-
-const Total = (props) => (
-  <p>Number of exercises: {props.total}</p>
-)
+const Total = (props) => {
+  let total = 0;
+  props.parts.forEach(part => {
+    total += part.exercises;
+  })
+  return (
+    <p>Number of exercises: {total}</p>
+  )
+}
 
 const Parts = (props) => (
   <p>
-    {props.part} {props.exercises}
+    {props.name} {props.exercises}
   </p>
 )
 export default App
